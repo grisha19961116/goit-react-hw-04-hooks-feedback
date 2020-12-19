39dc7cd21e73d981528c843eb3dbf02aba4a5520
unknown <grisha1996111616@gmail.com>
2020-12-19 11:03:41 +0000

    used hooks in feedback

--- src/components/FeedBack/FeedBack.js
+++ src/components/FeedBack/FeedBack.js
@@ -1,57 +1,63 @@
 import style from './FeedBack.module.css';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Statistics from './Statistics/Statistics.js';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions.js';
 import Section from './Section/Section.js';
 import Notification from './Notification/Notification.js';
 
-class FeedBack extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    total: 0,
-    percentage: 0,
-  };
-  countPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
-    return total ? Math.ceil((100 / total) * good) : 0;
-  };
-  handleFeedback = ({ target }) => {
+function FeedBack() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [percentage, setPercentage] = useState(0);
+
+  useEffect(() => {
+    setTotal(good + neutral + bad);
+  }, [good, neutral, bad]);
+
+  useEffect(() => {
+    setPercentage(Math.ceil((100 / total) * good));
+  }, [total, good]);
+
+  const handleFeedback = ({ target }) => {
     const { name } = target;
-    this.setState({
-      [name]: this.state[name] + 1,
-    });
+
+    switch (name) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      default:
+        console.log('Sorry, we are not have ' + name + '.');
+    }
   };
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
-  render() {
-    const { good, neutral, bad } = this.state;
-    const totalFn = this.countTotalFeedback();
-    const percentFn = this.countPercentage();
-    return (
-      <div className={style.wrapper}>
-        <Section title="Please leave feedback" flag={true}>
-          <FeedbackOptions onClick={this.handleFeedback} />
-        </Section>
-        <Section title="Statistics" flag={false}>
-          {totalFn > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={totalFn}
-              percentage={percentFn}
-            />
-          ) : (
-            <Notification />
-          )}
-        </Section>
-      </div>
-    );
-  }
+
+  return (
+    <div className={style.wrapper}>
+      <Section title="Please leave feedback" flag={true}>
+        <FeedbackOptions onClick={even => handleFeedback(even)} />
+      </Section>
+      <Section title="Statistics" flag={false}>
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            percentage={percentage}
+          />
+        ) : (
+          <Notification />
+        )}
+      </Section>
+    </div>
+  );
 }
+
 export default FeedBack;
